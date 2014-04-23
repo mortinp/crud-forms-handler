f5952e83deb391e9948a13b1469f658352c38580
unknown <mproenza@proenza.grm.desoft.cu>
2014-04-23 16:09:22 -0400

    db update

--- yotellevo.sql
+++ yotellevo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 22-04-2014 a las 03:09:12
+-- Tiempo de generación: 23-04-2014 a las 20:08:39
 -- Versión del servidor: 5.5.16
 -- Versión de PHP: 5.3.8
 
@@ -38,19 +38,20 @@ CREATE TABLE IF NOT EXISTS `drivers` (
   `active` tinyint(1) NOT NULL,
   `has_modern_car` tinyint(1) NOT NULL,
   `has_air_conditioner` tinyint(1) NOT NULL,
+  `contact_name` varchar(250) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Volcado de datos para la tabla `drivers`
 --
 
-INSERT INTO `drivers` (`id`, `username`, `password`, `max_people_count`, `active`, `has_modern_car`, `has_air_conditioner`) VALUES
-(7, 'eduartd@nauta.cu', 'b074e2f38af8d33d8026b4facf2a6bfc03e4f77f', 4, 0, 0, 0),
-(8, 'wary@dps.grm.sld.cu', 'f1ed9dc220787b7570dd4bf76f0b29205e55562a', 4, 0, 1, 1),
-(9, 'rricardo@grm.desoft.cu', '2773b7ee46895cf2b2f38fcb80f1403c1f136ec0', 4, 0, 1, 0),
-(10, 'nelson@ksabes.com', 'f83bf0b762b0eb17c78b944c77d1d3eb3149bc81', 4, 1, 1, 1);
+INSERT INTO `drivers` (`id`, `username`, `password`, `max_people_count`, `active`, `has_modern_car`, `has_air_conditioner`, `contact_name`) VALUES
+(7, 'eduartd@nauta.cu', 'b074e2f38af8d33d8026b4facf2a6bfc03e4f77f', 4, 0, 0, 0, 'Ernesto'),
+(8, 'wary@dps.grm.sld.cu', 'f1ed9dc220787b7570dd4bf76f0b29205e55562a', 4, 0, 1, 1, 'Wary'),
+(9, 'rricardo@grm.desoft.cu', '2773b7ee46895cf2b2f38fcb80f1403c1f136ec0', 4, 0, 1, 0, 'Nello'),
+(11, 'mproenza@grm.desoft.cu', '60dd56fce363a2e493ae60bfdc64a9dffb0b227b', 4, 1, 1, 1, 'Martín');
 
 -- --------------------------------------------------------
 
@@ -78,10 +79,6 @@ INSERT INTO `driver_localities` (`id`, `driver_id`, `locality_id`) VALUES
 (13, 7, 2),
 (14, 7, 3),
 (15, 7, 4),
-(16, 10, 1),
-(17, 10, 2),
-(18, 10, 3),
-(19, 10, 4),
 (20, 8, 1),
 (21, 8, 2),
 (22, 8, 3),
@@ -132,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `localities` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `localities_province_fk` (`province_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `localities`
@@ -159,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `pending_users` (
   `activation_id` varchar(250) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -173,7 +170,7 @@ CREATE TABLE IF NOT EXISTS `provinces` (
   `name` varchar(250) COLLATE latin1_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_bin AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_bin AUTO_INCREMENT=2 ;
 
 --
 -- Volcado de datos para la tabla `provinces`
@@ -205,16 +202,7 @@ CREATE TABLE IF NOT EXISTS `travels` (
   UNIQUE KEY `id` (`id`),
   KEY `travels_locality_fk` (`locality_id`),
   KEY `travels_user_fk` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=66 ;
-
---
--- Volcado de datos para la tabla `travels`
---
-
-INSERT INTO `travels` (`id`, `locality_id`, `destination`, `date`, `people_count`, `contact`, `user_id`, `state`, `drivers_sent_count`, `need_modern_car`, `need_air_conditioner`) VALUES
-(63, 3, 'dsffd', '2014-04-24', 3, 'fdsfd', 1, 'U', 0, 0, 0),
-(64, 1, 'dafdsf', '2014-04-25', 3, 'dfdsfsdf', 1, 'U', 0, 1, 1),
-(65, 4, 'fdafdsfds', '2014-04-26', 1, 'fds fdsfsdfdsfd', 1, 'U', 0, 0, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=74 ;
 
 -- --------------------------------------------------------
 
@@ -242,7 +230,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `active`, `display_name`, `created`) VALUES
 (1, 'ttt@ttt.ttt', '3a49921023b6c1d0a53cc864581e91f5f0e05109', 'regular', 1, 'martin', '0000-00-00'),
 (3, 'mproenza@grm.desoft.cu', '60dd56fce363a2e493ae60bfdc64a9dffb0b227b', 'admin', 1, '', '0000-00-00'),
-(11, 'nelson@ksabes.com', 'f83bf0b762b0eb17c78b944c77d1d3eb3149bc81', 'regular', 0, '', '2014-04-17');
+(11, 'nelson@ksabes.com', 'f83bf0b762b0eb17c78b944c77d1d3eb3149bc81', 'regular', 1, '', '2014-04-17');
 
 --
 -- Restricciones para tablas volcadas

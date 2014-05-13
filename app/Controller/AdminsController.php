a6d12422db34bc97347b910568d6bb96a9b12c52
unknown <mproenza@proenza.grm.desoft.cu>
2014-05-13 15:14:10 -0400

    admins/view_travels_by_email_log

---
+++ app/Controller/AdminsController.php
@@ -0,0 +1,20 @@
+<?php
+
+App::uses('AppController', 'Controller');
+App::uses('File', 'Utility');
+
+class AdminsController extends AppController {
+    
+    public function view_travels_by_email_log() {
+        $file = new File('../tmp/logs/viaje_por_correo.log');
+        
+        $lines = preg_split("/(\r\n|\n|\r)/", $file->read());
+        $file_content = '';
+        foreach ($lines as $l) {
+            $file_content .= $l.'<br/>';
+        }
+        $this->set('content', $file_content);
+    }
+}
+
+?>

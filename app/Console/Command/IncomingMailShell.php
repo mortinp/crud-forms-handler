1a63cbfe5891a7af7a908a9ad36e019652267fcd
Martín <mproenza@proenza.grm.desoft.cu>
2014-05-20 16:48:55 -0400

    tbe updates

--- app/Console/Command/IncomingMailShell.php
+++ app/Console/Command/IncomingMailShell.php
@@ -31,51 +31,20 @@ class IncomingMailShell extends AppShell {
     }
     
     public function process2() {
-        /*$email = "Return-Path: <manuel@ksabes.com>
-            Delivered-To: viajes@yotellevo.ahiteva.net
-            Received: from h0.host.net (HELO queue) (123.123.33.50)
-                            by h0.host.net with SMTP; 10 May 2012 16:41:46 +0200
-            Received: from mx.aruba.it (321.321.157.29)
-              by mx.host.net with SMTP; 10 May 2012 16:41:44 +0200
-            Received: (qmail 11142 invoked by uid 89); 10 May 2012 14:41:43 -0000
-            Date: 10 May 2012 14:41:43 -0000
-            Message-ID: <1336660903.11137.blah@host.it>
-            Delivered-To: Autoresponder
-            To: viajes@yotellevo.ahiteva.net
-            From: manuel@ksabes.com
-            Subject: Bayamo-Habana
-            Subject1: Re: =?UTF-8?Q?Testo=20Del=20di=20Soggetto=20Che=20?=
-                            =?UTF-8?Q?Va=20A=20Capo=20In=20UTF8=20?=
-            X-Spam-Check: DONE|U 0.500569/N
-            X-Spam-Check: OK
-
-            Da: info@domain.it
-            Oggetto: Grazie!!!
-
-            Nome Cognome
-            ";*/
-        
-        CakeLog::write('viaje_por_correo', 'Email Received');
-        $this->out('Email Received');
+        /*CakeLog::write('viaje_por_correo', 'Email Received');
+        $this->out('Email Received');*/
         
         $stdin = fopen('php://stdin', 'r');
-        //$email = trim(fgets(STDIN));
-        $emailParser = new PlancakeEmailParser(stream_get_contents($stdin)/*$email*/);
+        $emailParser = new PlancakeEmailParser(stream_get_contents($stdin));
         fclose($stdin);
         
         // TODO: Verificar el formato del to, segun lo que me dijo Manuel
-        $target = $emailParser->getTo();
-        //CakeLog::write('viaje_por_correo', 'target: '.$target);
-        foreach ($target as $key => $value) {
-            CakeLog::write('viaje_por_correo', $key.'=>'.$value);
-        }
-        //$this->out('target: '.$target);
-        
+        $target = $emailParser->getTo();        
         $to = $target[0];
         $to = str_replace('<', '', $to);
         $to = str_replace('>', '', $to);
-        CakeLog::write('viaje_por_correo', 'to: '.$to);
-        $this->out('to: '.$to);
+        /*CakeLog::write('viaje_por_correo', 'to: '.$to);
+        $this->out('to: '.$to);*/
         
         if($to === 'viajes@yotellevo.ahiteva.net') {
             
@@ -84,27 +53,27 @@ class IncomingMailShell extends AppShell {
             $sender = $match[1];
             if($sender == null || strlen($sender) == 0) $sender = $text;
             //$sender = $emailParser->getHeader('From');
-            CakeLog::write('viaje_por_correo', 'sender: '.$sender);
-            $this->out('sender: '.$sender);
+            /*CakeLog::write('viaje_por_correo', 'sender: '.$sender);
+            $this->out('sender: '.$sender);*/
             
             $subject = trim($emailParser->getSubject());
             $subject = str_replace("'", "", $subject);
             $subject = str_replace('"', "", $subject);
-            CakeLog::write('viaje_por_correo', 'subject: '.$subject);
-            $this->out('subject: '.$subject);
+            /*CakeLog::write('viaje_por_correo', 'subject: '.$subject);
+            $this->out('subject: '.$subject);*/
 
             // TODO: Verificar que origen y destino se pudieron sacar del asunto
             preg_match('/(?<from>.+)-(?<to>.+)/', $subject, $matches);
             $origin = $matches['from'];
             $destination = $matches['to']; 
-            CakeLog::write('viaje_por_correo', 'origin: '.$origin);
+            /*CakeLog::write('viaje_por_correo', 'origin: '.$origin);
             $this->out('origin: '.$origin);
             CakeLog::write('viaje_por_correo', 'destination: '.$destination);
-            $this->out('destination: '.$destination);
+            $this->out('destination: '.$destination);*/
 
             $description = $emailParser->getPlainBody();
-            CakeLog::write('viaje_por_correo', 'body: '.$description);
-            $this->out('body: '.$description);
+            /*CakeLog::write('viaje_por_correo', 'body: '.$description);
+            $this->out('body: '.$description);*/
 
             /*$now = date("Y-m-d H:i:s");
             $log = fopen('/tmp/email_receiver.log', 'a');
@@ -147,7 +116,7 @@ class IncomingMailShell extends AppShell {
             foreach ($thesaurus as $t) {
                 
                 $target = $t['LocalityThesaurus']['fake_name'];
-                $this->out($t['LocalityThesaurus']['fake_name']);
+                //$this->out($t['LocalityThesaurus']['fake_name']);
                 
                 $result = $this->match($origin, $destination, $target, $shortest);
                 if($result != null && !empty ($result)) {
@@ -206,88 +175,6 @@ class IncomingMailShell extends AppShell {
             $result = $this->TravelLogic->confirmTravel('TravelByEmail', $travel);
             
             $OK = $result['success'];
-            
-            /*if(isset ($closest['locality_id'])) {
-                $drivers = $this->DriverLocality->find('all', array('conditions'=>
-                    array(
-                        'DriverLocality.locality_id'=>$closest['locality_id'],
-                        'Driver.active'=>true
-                        )
-                    ));                
-            } else if(isset ($closest['municipalities'])) {
-                // TODO: Buscar en todos los municipios de la provincia
-                foreach ($closest['municipalities'] as $id => $name) {
-                    
-                }
-                $drivers = array();
-                
-            } else {
-                $OK = false;
-            }
-            //$this->out(print_r($drivers, true));
-            
-            
-            if($OK) {
-                if(count($drivers) > 0) {
-                    $travel = array('TravelByEmail');
-                    $travel['TravelByEmail']['user_origin'] = $origin;
-                    $travel['TravelByEmail']['user_destination'] = $destination;
-                    $travel['TravelByEmail']['description'] = $description;
-                    $travel['TravelByEmail']['matched'] = $closest['name'];
-                    $travel['TravelByEmail']['locality_id'] = $closest['locality_id'];
-                    $travel['TravelByEmail']['where'] = $closest['direction'] == 0? $destination : $origin;
-                    $travel['TravelByEmail']['direction'] = $closest['direction'];
-                    $travel['TravelByEmail']['user_id'] = $userId;
-                    $travel['TravelByEmail']['state'] = Travel::$STATE_CONFIRMED;
-                    $travel['TravelByEmail']['drivers_sent_count'] = count($drivers);
-                    if($this->TravelByEmail->save($travel)) {
-                        $travel_id = $this->TravelByEmail->getLastInsertID();
-                    } else {
-                        $OK = false;
-                    }
-                } else {
-                    $this->out('No se encontraron choferes');
-                    $OK = false;
-                }
-                
-                $drivers_sent_count = 0;
-                if($OK) {
-                    // Enviar a los choferes
-                    foreach ($drivers as $d) {
-                        $travel['Locality'] = array('id'=>$closest['locality_id'], 'name'=>$closest['name']);
-                        
-                        if(Configure::read('enqueue_mail')) {
-                            ClassRegistry::init('EmailQueue.EmailQueue')->enqueue(
-                                    $d['Driver']['username'],
-                                    array('travel' => $travel), 
-                                    array(
-                                        'template'=>'new_travel_by_email', 
-                                        'format'=>'html',
-                                        'subject'=>'Nuevo Anuncio de Viaje (#'.$travel_id.' por correo)',
-                                        'config'=>'no_responder')
-                                    );
-                        } else {
-                            $Email = new CakeEmail('no_responder');
-                            $Email->template('new_travel_by_email')
-                            ->viewVars(array('travel' => $travel))
-                            ->emailFormat('html')
-                            ->to($d['Driver']['username'])
-                            ->subject('Nuevo Anuncio de Viaje (#'.$travel_id.' por correo)');
-                            try {
-                                $Email->send();
-                            } catch ( Exception $e ) {
-                                if($drivers_sent_count < 1) {
-                                    //$this->setErrorMessage('Ocurrió un error enviando el viaje a los choferes. Intenta de nuevo.');
-                                    $OK = false;
-                                    continue;
-                                }
-                            }
-                        }                        
-                        
-                        $drivers_sent_count++;
-                    }
-                }                
-            }*/
             
         } else {
             $OK = false;
